8c5490ccdde0694eb2135175f6052dcbc7fca651
Ueder Carlos Costa Caetano <uedercosta@gmail.com>
2026-08-30 22:41:21 -0300

    Criando o projeto - Desafio Helpdesk

---
+++ src/test/java/br/com/techagro/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techagro.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
